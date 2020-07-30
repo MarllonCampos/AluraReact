c97164be83d18d30fb0755a171da50a4420e6305
MarllonCampos <marlloncampos@outlook.com>
2020-07-30 12:54:26 -0300

    Criado component pagina404 e pagina categoria

--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './pages/home'
 import CadastroVideo from './pages/cadastro/video'
-
-function PageNotFound(){
-    return(
-        <div>
-           Página 404
-        </div>
-    )
-}
+import CadastroCategoria from './pages/cadastro/categoria';
+import PageNotFound from './pages/page404';
 
 ReactDOM.render( 
     <BrowserRouter>
         <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/cadastro/video" component={CadastroVideo} />
+            <Route path="/cadastro/categoria" component={CadastroCategoria} />
             <Route component={PageNotFound} />
         </Switch>
     </BrowserRouter>,
